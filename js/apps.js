d22361d9b9e02e36fe2e764128d565104f542a3b
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2018-12-19 19:46:24 +0000

    Base game functionality

--- js/apps.js
+++ js/apps.js
@@ -2,37 +2,41 @@ console.log("loaded");
 
 document.addEventListener( "DOMContentLoaded", function(){
 
-  console.log("Hello");
-  var squares = document.getElementsByClassName("td")
 
-  squares.addEventListener("click", function(event){
-      console.log( "was clicked");
-    });
+  var square = document.body.children[2];
+  var currentTurn = document.body.children[1]
+  var turn_count = 1
+  var boxes = document.getElementsByTagName("td")
 
+  for (var i = 0; i < boxes.length; i++) {
+    box = boxes[i]
+    box.addEventListener("click", function(event){
+      if (this.innerHTML != "") {
+        alert("Invalid move, pick again")
+      }
+      else {
+        if (turn_count % 2 != 0) {
+          this.classList.add("X")
+          this.innerHTML= "X"
 
+          } else {
+          this.classList.add("O")
+          this.innerHTML= "O"
+          }
+        updateturn(currentTurn)
+      }
+    })
+  }
+  function updateturn(turn){
+    turn_count = turn_count + 1
+    if (turn_count % 2 != 0) {
+      turn.innerHTML = "It is X's turn"
+    } else {
+      turn.innerHTML = "It is O's turn"
+    }
+  }
+  function wincheck(){
+    
+  }
 
-
-  //   for (var i = 0; i < squares.length; i++) {
-  //     var square = squares[i]
-  //     square.addEventListener("click", function(event){
-  //     console.log(this.innerHTML + " was clicked");
-  //   });
-  //   }
-  // });
-
-
-  // var div1= document.getElementById("div1")
-  // var div2= document.getElementById("div2")
-  // var div3= document.getElementById("div3")
-  //
-  // div1.addEventListener("click", function(event){
-  //   console.log("div1 was clicked");
-  // });
-  // div2.addEventListener("click", function(event){
-  //   console.log("div2 was clicked");
-  //   event.stopPropagation()
-  // });
-  // div3.addEventListener("click", function(event){
-  //   console.log("div3 was clicked");
-  // });
 });
