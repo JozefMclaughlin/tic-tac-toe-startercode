dc2c9493e9523f71ddfcaa1de784b4d27cee72a9
tech-a47 <tech-a47@Tech-A47s-MacBook-Air.local>
2018-12-19 16:12:00 +0000

    More setting up

--- js/apps.js
+++ js/apps.js
@@ -1,8 +1,38 @@
 console.log("loaded");
+
 document.addEventListener( "DOMContentLoaded", function(){
 
+  console.log("Hello");
+  var squares = document.getElementsByClassName("td")
+
+  squares.addEventListener("click", function(event){
+      console.log( "was clicked");
+    });
 
 
 
 
+  //   for (var i = 0; i < squares.length; i++) {
+  //     var square = squares[i]
+  //     square.addEventListener("click", function(event){
+  //     console.log(this.innerHTML + " was clicked");
+  //   });
+  //   }
+  // });
+
+
+  // var div1= document.getElementById("div1")
+  // var div2= document.getElementById("div2")
+  // var div3= document.getElementById("div3")
+  //
+  // div1.addEventListener("click", function(event){
+  //   console.log("div1 was clicked");
+  // });
+  // div2.addEventListener("click", function(event){
+  //   console.log("div2 was clicked");
+  //   event.stopPropagation()
+  // });
+  // div3.addEventListener("click", function(event){
+  //   console.log("div3 was clicked");
+  // });
 });
